30e1c4c789050b94296b8a3ea6663a50b4c4f05e
greinerchan <greinerchan@gmail.com>
2020-08-10 19:46:02 -0400

    per scholas exercise

---
+++ week3/code/exercise.javascript_printing-shapes-master/assets/js/shapes.js
@@ -0,0 +1,34 @@
+function getLine(length) {
+    // TODO - write method definition here    
+    var astericks = "*";
+    console.log(astericks.repeat(length));
+}
+
+getLine(30);
+
+
+function getBox(width, height) {
+    // TODO - write method definition here
+}
+
+
+
+function getBottomLeftTriangle(length) {
+    // TODO - write method definition here
+}
+
+
+function getUpperLeftTriangle(length) {
+    // TODO - write method definition here
+}
+
+
+
+function getPyramid(length) {
+    // TODO - write method definition here
+}
+
+
+function getCheckerboard(width, height) {
+    // TODO - write method definition here
+}
